c858c040d90b70a2c335ccc170ea20395ab00cb5
Ihsan946 <ihsanmuhammad946@gmail.com>
2021-04-27 14:15:18 +0700

    Initial commit

---
+++ app/src/main/java/com/app/ihsan946/tugas3akb/Halaman1Activity.java
@@ -0,0 +1,14 @@
+package com.app.ihsan946.tugas3akb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Halaman1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman1);
+    }
+}
