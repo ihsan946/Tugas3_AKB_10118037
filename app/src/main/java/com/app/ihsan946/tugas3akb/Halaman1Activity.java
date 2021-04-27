8700e5887dc9200c4609059939069c571d2bc337
Ihsan946 <ihsanmuhammad946@gmail.com>
2021-04-28 06:27:48 +0700

    tampilan Login dan transisi dari halaman1 ke Login

--- app/src/main/java/com/app/ihsan946/tugas3akb/Halaman1Activity.java
+++ app/src/main/java/com/app/ihsan946/tugas3akb/Halaman1Activity.java
@@ -2,7 +2,10 @@ package com.app.ihsan946.tugas3akb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class Halaman1Activity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class Halaman1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman1);
+    }
+
+    public void pindahLogin(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
     }
 }
