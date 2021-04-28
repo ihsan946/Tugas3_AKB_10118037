1e53377d63c6dc2ef8707cb947595c507c4ce24a
Ihsan946 <ihsanmuhammad946@gmail.com>
2021-04-28 17:40:14 +0700

    tampilan biodata dan transisi dari login ke biodata

---
+++ app/src/main/java/com/app/ihsan946/tugas3akb/BiodataActivity.java
@@ -0,0 +1,14 @@
+package com.app.ihsan946.tugas3akb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BiodataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biodata);
+    }
+}
--- app/src/main/java/com/app/ihsan946/tugas3akb/LoginActivity.java
+++ app/src/main/java/com/app/ihsan946/tugas3akb/LoginActivity.java
@@ -2,7 +2,12 @@ package com.app.ihsan946.tugas3akb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.text.InputFilter;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        EditText kodelogin = findViewById(R.id.kode_login);
+
+        //All capslock kode login
+        kodelogin.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
+    }
+
+    public void pindahBiodata(View view) {
+        Intent intent = new Intent(this, BiodataActivity.class);
+        startActivity(intent);
+
     }
 }
